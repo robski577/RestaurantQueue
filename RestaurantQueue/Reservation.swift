7fc96f93df19a0d35ecf20d22e74b9e8f77fb3fe
FreeMasen <RFM@localhost>
2016-04-24 14:50:53 -0500

    updated reservation model to be available to both apps, changed waitTime to arrivalTime, added constructor, updated view to no longer look for wait time.

--- RestaurantQueue/Reservation.swift
+++ RestaurantQueue/Reservation.swift
@@ -12,5 +12,11 @@ import Foundation
 struct Reservation {
     var name: String
     var size: Int
-    var waitTime: Int
+    var arrivalTime: NSDate
+    
+    init(name: String, size: Int, arrivalTime: NSDate = NSDate()) {
+        self.name = name
+        self.size = size
+        self.arrivalTime = arrivalTime
+    }
 }
